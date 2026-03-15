bfd11cfe4d80d711730327a5d5f3146f08dc20f8
Sirathee Koomgreng <ska514@sfu.ca>
2026-03-14 20:21:08 -0700

    disable force destroy

--- infrastructure/gcs.tf
+++ infrastructure/gcs.tf
@@ -6,7 +6,6 @@ locals {
 resource "google_storage_bucket" "main_data" {
   name                        = local.main_data_bucket_name
   location                    = "US-WEST1"
-  force_destroy               = true
   public_access_prevention    = "enforced"
   uniform_bucket_level_access = true
   hierarchical_namespace {
